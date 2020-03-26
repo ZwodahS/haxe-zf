65497fc2e748da6f5336bb037245503169c6cba2
Eric Ng <ericnjf@gmail.com>
2020-03-27 00:22:40 +0800

    feat/gridutils: add circle and line algo

--- GridUtils.hx
+++ GridUtils.hx
@@ -63,4 +63,91 @@ class GridUtils {
     public static inline function getItemIn2DArray<T>(pos: Point2i, grid: Vector<Vector<T>>): T {
         return (inGridArray(pos, grid)) ? grid[pos.x][pos.y] : null;
     }
+
+    public static function getLine(start: Point2i, end: Point2i): List<Point2i> {
+        // http://www.roguebasin.com/index.php?title=Bresenham%27s_Line_Algorithm
+        // modified from python version
+        var x1 = start.x, y1 = start.y;
+        var x2 = end.x, y2 = end.y;
+        var isSteep = hxd.Math.iabs(y2-y1) > hxd.Math.iabs(x2-x1);
+
+        var tmp: Int = 0;
+        if (isSteep) {
+            tmp = x1; x1 = y1; y1 = tmp;
+            tmp = x2; x2 = y2; y2 = tmp;
+        }
+
+        var reversed = false;
+        if (x1 > x2) {
+            tmp = x1; x1 = x2; x2 = tmp;
+            tmp = y1; y1 = y2; y2 = tmp;
+            reversed = true;
+        }
+
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+
+        var errorValue: Int = Std.int(dx / 2.0);
+        var yStep = -1;
+        if (y1 < y2) {
+            yStep = 1;
+        }
+
+        var points = new List<Point2i>();
+        var insertFunc = reversed ? points.add : points.push;
+        var y = y1, x = x1;
+        while (x < x2+1) {
+            var point: Point2i = null;
+            if (isSteep) {
+                point = new Point2i(y, x);
+            } else {
+                point = new Point2i(x, y);
+            }
+            insertFunc(point);
+            errorValue -= hxd.Math.iabs(dy);
+            if (errorValue < 0) {
+                y += yStep;
+                errorValue += dx;
+            }
+
+            x++;
+        }
+        return points;
+    }
+
+    public static function getCircle(center: Point2i, radius: Int): List<Point2i> {
+        // https://en.wikipedia.org/wiki/Midpoint_circle_algorithm
+        var x0 = center.x, y0 = center.y;
+        var x = radius;
+        var y = 0;
+        var err = 0;
+
+        var pointsSet = new Map<String, Point2i>();
+
+        while (x >= y) {
+            pointsSet['${x0 + x}_${y0 + y}'] = new Point2i(x0 + x, y0 + y);
+            pointsSet['${x0 + y}_${y0 + x}'] = new Point2i(x0 + y, y0 + x);
+            pointsSet['${x0 - y}_${y0 + x}'] = new Point2i(x0 - y, y0 + x);
+            pointsSet['${x0 - x}_${y0 + y}'] = new Point2i(x0 - x, y0 + y);
+            pointsSet['${x0 - x}_${y0 - y}'] = new Point2i(x0 - x, y0 - y);
+            pointsSet['${x0 - y}_${y0 - x}'] = new Point2i(x0 - y, y0 - x);
+            pointsSet['${x0 + y}_${y0 - x}'] = new Point2i(x0 + y, y0 - x);
+            pointsSet['${x0 + x}_${y0 - y}'] = new Point2i(x0 + x, y0 - y);
+
+            if (err <= 0) {
+                y++;
+                err += 2*y + 1;
+            }
+            if (err > 0) {
+                x--;
+                err -= 2*x + 1;
+            }
+        }
+
+        var points = new List<Point2i>();
+        for (point in pointsSet) {
+            points.push(point);
+        }
+        return points;
+    }
 }
