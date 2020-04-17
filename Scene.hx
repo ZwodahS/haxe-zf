4490bfc4dda6a0bfcbc1b3af737db81bc1540382
Eric Ng <ericnjf@gmail.com>
2020-04-17 13:35:43 +0800

    feat/scene: add a destroy function to scene

--- Scene.hx
+++ Scene.hx
@@ -5,4 +5,5 @@ class Scene {
     public function update(dt: Float): Void {}
     public function render(engine: h3d.Engine): Void {}
     public function onEvent(event: hxd.Event): Void {}
+    public function destroy(): Void {}
 }
