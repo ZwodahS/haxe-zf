e9111c67a77e0d97087f8ea9d28cb3d8020f1d86
Co <nhatquang238@gmail.com>
2020-04-20 00:37:32 +0800

    feat/Factory: add maxWidth support for text creation

--- Factory.hx
+++ Factory.hx
@@ -7,13 +7,15 @@ class Factory {
             font: h2d.Font,
             text: String,
             position: Point2f = null,
-            color: h3d.Vector = null) {
+            color: h3d.Vector = null,
+            maxWidth: Int = null) {
 
         var t = new h2d.Text(font);
         t.text = text;
 
         if (position != null) { t.x = position.x; t.y = position.y; }
         if (color != null) t.color = color;
+        if (maxWidth != null) t.maxWidth = maxWidth;
 
         // complicated logic
 
