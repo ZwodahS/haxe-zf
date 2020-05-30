87fe4258d61b262f67c3c1ceb72a3d1c6c3bdec7
Eric Ng <ericnjf@gmail.com>
2020-05-30 20:52:38 +0800

    feat/list-utils: add list utils to shuffle list via arrayutils

---
+++ ListUtils.hx
@@ -0,0 +1,13 @@
+
+package common;
+
+class ListUtils {
+
+    // this uses Array utils shuffle
+    public static function shuffle<T>(list: List<T>, random: hxd.Rand = null) {
+        var arr = [for (item in list) item];
+        ArrayUtils.shuffle(arr);
+        list.clear();
+        for (item in arr) list.push(item);
+    }
+}
