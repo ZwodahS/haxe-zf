e5f1cb5d54332d6bc8cfa680566a990ff325e436
Eric Ng <ericnjf@gmail.com>
2020-06-10 15:42:27 +0800

    feat/wrappedValue: add wrapped value for easy data binding

---
+++ WrappedValue.hx
@@ -0,0 +1,34 @@
+
+package common;
+
+class WrappedValue<T> {
+
+    public var value(default, set): T;
+
+    var listeners: Map<Int, (T, T) -> Void>;
+    var listenerCounter:Int = 0;
+
+    public function new(defaultValue: T) {
+        this.listeners = new Map<Int, (T, T) -> Void>();
+        this.value = defaultValue;
+    }
+
+    public function set_value(value: T): T {
+        var oldValue = this.value;
+        this.value = value;
+        for (l in this.listeners) {
+            l(oldValue, this.value);
+        }
+        return this.value;
+    }
+
+    public function listen(func: (T, T) -> Void): Int {
+        var id = listenerCounter++;
+        this.listeners[id] = func;
+        return id;
+    }
+
+    public function removeListener(id: Int) {
+        this.listeners.remove(id);
+    }
+}
