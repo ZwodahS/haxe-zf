009055a713ee4570c27394f98e30af151139eadf
Eric Ng <ericnjf@gmail.com>
2020-09-07 16:01:02 +0800

    feat/animations: return the Animation after runAnim

--- animations/Animator.hx
+++ animations/Animator.hx
@@ -5,10 +5,11 @@ class Animator extends common.Updater { // extends the Updater since most of it 
         super();
     }
 
-    public function runAnim(anim: Animation, onFinish: Void->Void = null) {
+    public function runAnim(anim: Animation, onFinish: Void->Void = null): Animation {
         if (onFinish != null) anim.onFinish = onFinish;
         this.run(anim);
         anim.animator = this;
+        return anim;
     }
 
     public function wait(duration: Float, func: Void->Void) {
