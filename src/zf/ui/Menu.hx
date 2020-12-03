9346083a78e99d3514a9b2a44c0288bb82d2107a
Eric Ng <ericnjf@gmail.com>
2020-12-03 22:25:33 +0800

    feat/ui/Menu: add a simple Menu ui container

---
+++ src/zf/ui/Menu.hx
@@ -0,0 +1,112 @@
+package zf.ui;
+
+/**
+    Provide a data container for
+
+    1. Storing menu items
+    2. Manipulating menu item
+    3. Performing action on the menu item
+**/
+class MenuItem extends h2d.Object {
+    public var selected(default, set): Bool;
+
+    public function set_selected(s: Bool): Bool {
+        return this.selected = s;
+    }
+
+    public function activate() {}
+}
+
+class MenuList extends h2d.Object {
+    public var items: Array<MenuItem>;
+
+    public var wrapped: Bool = true;
+
+    public var selectedIndex(default, set): Int = -1;
+
+    public function set_selectedIndex(i: Int): Int {
+        var prev = this.selectedIndex;
+        this.selectedIndex = i;
+        updateSelect(prev, this.selectedIndex);
+        return this.selectedIndex;
+    }
+
+    public var selectedItem(get, never): MenuItem;
+
+    public function get_selectedItem(): MenuItem {
+        return (selectedIndex < 0 || selectedIndex >= items.length) ? null : items[selectedIndex];
+    }
+
+    public function new() {
+        super();
+        this.items = [];
+    }
+
+    public function previous() {
+        if (outOfBound(selectedIndex)) return;
+        var prevIndex = selectedIndex;
+        selectedIndex -= 1;
+        if (selectedIndex < 0) selectedIndex = (wrapped ? items.length - 1 : 0);
+        updateSelect(prevIndex, selectedIndex);
+    }
+
+    public function next() {
+        if (outOfBound(selectedIndex)) return;
+        var prevIndex = selectedIndex;
+        selectedIndex += 1;
+        if (selectedIndex >= items.length) selectedIndex = (wrapped ? 0 : items.length - 1);
+        updateSelect(prevIndex, selectedIndex);
+    }
+
+    function updateSelect(prev, curr) {
+        if (prev == curr) return; // no change
+        if (!outOfBound(prev)) {
+            items[prev].selected = false;
+        }
+        if (!outOfBound(curr)) {
+            items[curr].selected = true;
+        }
+    }
+
+    function outOfBound(i: Int): Bool {
+        return i < 0 || i >= items.length;
+    }
+
+    // activate the current item
+    public function activate() {
+        if (selectedItem == null) return;
+        selectedItem.activate();
+    }
+
+    public function clear() {
+        this.items = [];
+        this.selectedIndex = -1;
+    }
+
+    public function addItem(item: MenuItem) {
+        this.items.push(item);
+    }
+}
+
+class VerticalMenuList extends MenuList {
+    var itemHeight: Int;
+    var itemSpacing: Int;
+
+    public function new(itemHeight: Int, itemSpacing: Int) {
+        super();
+        this.itemHeight = itemHeight;
+        this.itemSpacing = itemSpacing;
+    }
+
+    override public function addItem(item: MenuItem) {
+        super.addItem(item);
+        item.y = (this.items.length * itemHeight) + ((this.items.length - 1) * itemSpacing);
+        item.selected = false;
+        this.addChild(item);
+    }
+
+    override public function clear() {
+        for (i in this.items) i.remove();
+        super.clear();
+    }
+}
