9bd6d7668042a84de8323530ae3014384d8eac16
Eric Ng <ericnjf@gmail.com>
2021-01-06 15:07:36 +0800

    feat/Logger: add Logger

---
+++ src/zf/Logger.hx
@@ -0,0 +1,42 @@
+package zf;
+
+import haxe.macro.PositionTools;
+import haxe.macro.Context;
+
+class Logger {
+    macro public static function error(msg: String) {
+#if (!loggingLevel || loggingLevel < 10)
+        return macro {};
+#else
+        var location = PositionTools.toLocation(Context.currentPos());
+        var locationString = location.file + ":" + location.range.start.line;
+        return macro {
+            haxe.Log.trace('${locationString}: [Error] ${msg}', null);
+        }
+#end
+    }
+
+    macro public static function info(msg: String) {
+#if (!loggingLevel || loggingLevel < 20)
+        return macro {};
+#else
+        var location = PositionTools.toLocation(Context.currentPos());
+        var locationString = location.file + ":" + location.range.start.line;
+        return macro {
+            haxe.Log.trace('${locationString}: [Info] ${msg}', null);
+        }
+#end
+    }
+
+    macro public static function debug(msg: String) {
+#if (!loggingLevel || loggingLevel < 30)
+        return macro {};
+#else
+        var location = PositionTools.toLocation(Context.currentPos());
+        var locationString = location.file + ":" + location.range.start.line;
+        return macro {
+            haxe.Log.trace('${locationString}: [Debug] ${msg}', null);
+        }
+#end
+    }
+}
