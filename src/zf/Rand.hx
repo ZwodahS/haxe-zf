74407213333c1adf28a6cf7ae2d8a2b97c5cb538
Eric Ng <ericnjf@gmail.com>
2021-01-18 21:31:06 +0800

    feat/Rand: convert Rand to abstract

--- src/zf/Rand.hx
+++ src/zf/Rand.hx
@@ -3,8 +3,20 @@ package zf;
 /**
     Extends hxd.Rand to provide additional functionality.
 **/
-class Rand extends hxd.Rand {
+@:forward(random, shuffle, rand, srand)
+abstract Rand(hxd.Rand) from hxd.Rand to hxd.Rand {
+    public function new(r: hxd.Rand) {
+        this = r;
+    }
+
     inline public function randomChoice<T>(a: Array<T>): Null<T> {
         return a.length == 0 ? null : a[this.random(a.length)];
+    }
+
+    inline public function randomPop<T>(a: Array<T>): Null<T> {
+        if (a.length == 0) return null;
+        var pos = this.random(a.length);
+        var item = a.splice(pos, 1);
+        return item[0];
     }
 }
