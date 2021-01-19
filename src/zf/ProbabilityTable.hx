ae250d55da0363b933907ee389962b17133da296
Eric Ng <ericnjf@gmail.com>
2021-01-19 22:14:40 +0800

    feat/ProbabilityTable: expose totalChance and length

--- src/zf/ProbabilityTable.hx
+++ src/zf/ProbabilityTable.hx
@@ -6,21 +6,28 @@ package zf;
 **/
 class ProbabilityTable<T> {
     var chances: Array<{chance: Int, item: T}>;
-    var total: Int;
+
+    public var totalChance(default, null): Int;
+
+    public var length(get, never): Int;
+
+    public function get_length(): Int {
+        return chances.length;
+    }
 
     public function new() {
         this.chances = [];
-        this.total = 0;
+        this.totalChance = 0;
     }
 
     public function add(chance: Int, item: T) {
         this.chances.push({chance: chance, item: item});
-        this.total += chance;
+        this.totalChance += chance;
     }
 
     public function roll(?r: hxd.Rand): Null<T> {
-        if (total == 0) return null;
-        var chance = r == null ? Random.int(1, total) : 1 + r.random(total);
+        if (totalChance == 0) return null;
+        var chance = r == null ? Random.int(1, totalChance) : 1 + r.random(totalChance);
         for (c in this.chances) {
             if (chance <= c.chance) return c.item;
             chance -= c.chance;
