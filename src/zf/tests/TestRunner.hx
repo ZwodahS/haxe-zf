b70ef45cc7e4173a12f52e9008dc906ad7df4475
Eric Ng <ericnjf@gmail.com>
2021-02-07 14:54:33 +0800

    feat/tests: add TestRunner

---
+++ src/zf/tests/TestRunner.hx
@@ -0,0 +1,18 @@
+package zf.tests;
+
+class TestRunner {
+    var cases: List<TestCase>;
+    public function new() {
+        this.cases = new List<TestCase>();
+    }
+
+    public function add(tc: TestCase) {
+        this.cases.add(tc);
+    }
+
+    public function run() {
+        for (tc in this.cases) {
+            tc.run();
+        }
+    }
+}
