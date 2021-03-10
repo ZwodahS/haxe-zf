89fd09f559516155894e35f52d2ab9608047665f
Eric Ng <ericnjf@gmail.com>
2021-03-10 20:44:12 +0800

    fix/TextButton: use width and height from Text

--- src/zf/ui/TextButton.hx
+++ src/zf/ui/TextButton.hx
@@ -8,26 +8,25 @@ class TextButton extends h2d.Object {
 	var label: h2d.HtmlText;
 	var text: String;
 
-	var width: Int;
-	var height: Int;
+	var width: Float;
+	var height: Float;
 	var defaultColor: Int;
 	var hoverColor: Int;
 	var isHovered: Bool = false;
 
-	public function new(width: Int, height: Int, defaultColor: Int, hoverColor: Int, text: String,
-			font: h2d.Font) {
+	public function new(defaultColor: Int, hoverColor: Int, text: String, font: h2d.Font) {
 		super();
 		this.text = text;
 		this.label = new h2d.HtmlText(font);
 		this.defaultColor = defaultColor;
 		this.hoverColor = hoverColor;
-		this.width = width;
-		this.height = height;
 
 		updateText();
 
 		this.addChild(label);
 
+		this.width = this.label.textWidth;
+		this.height = this.label.textHeight;
 		var interactive = new h2d.Interactive(width, height, this);
 		interactive.onOver = function(e: hxd.Event) {
 			this.isHovered = true;
@@ -46,7 +45,6 @@ class TextButton extends h2d.Object {
 	function updateText() {
 		var color = this.isHovered ? this.hoverColor : this.defaultColor;
 		this.label.text = '${this.text.font(color)}';
-		this.label.x = AlignmentUtils.center(0, this.width, this.label.textWidth);
 	}
 
 	dynamic public function onClick() {}
