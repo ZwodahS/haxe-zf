643a5bf4920fdaae3da62f0ecf8ceb1157fb64a8
Eric Ng <ericnjf@gmail.com>
2021-04-20 01:50:56 +0800

    feat/RandExtensions: add randomChoices without replacement

--- src/zf/RandExtensions.hx
+++ src/zf/RandExtensions.hx
@@ -8,6 +8,18 @@ class RandExtensions {
 		return a.length == 0 ? null : a[r.random(a.length)];
 	}
 
+	public static function randomChoices<T>(r: hxd.Rand, a: Array<T>, count: Int): Array<T> {
+		var choices = [for (i in 0...a.length) i];
+		var out: Array<T> = [];
+		for (i in 0...count) {
+			var choice = randomChoice(r, choices);
+			choices.remove(choice);
+			out.push(a[choice]);
+			if (choices.length == 0) break;
+		}
+		return out;
+	}
+
 	public static function randomPop<T>(r: hxd.Rand, a: Array<T>): Null<T> {
 		if (a.length == 0) return null;
 		var pos = r.random(a.length);
@@ -22,6 +34,7 @@ class RandExtensions {
 	}
 
 	public static function randomChance(r: hxd.Rand, chance: Int, base: Int = 100): Bool {
+		if (chance >= base) return true;
 		return r.random(base) < chance;
 	}
 }
