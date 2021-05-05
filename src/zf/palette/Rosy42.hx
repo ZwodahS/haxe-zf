35d839cc71ccf761b756d2ad3ab32b59485d6651
Eric Ng <ericnjf@gmail.com>
2021-05-05 17:01:33 +0800

    feat/Rosy42: add color palette for rosy42

---
+++ src/zf/palette/Rosy42.hx
@@ -0,0 +1,62 @@
+package zf.palette;
+
+/**
+	Extended Rosy Color palette.
+**/
+class Rosy42 {
+	// store the colors in the same order as the color palette in aseprite
+	// this is to allow for not having to enter color code manually
+	// @formatter:off
+	public static final Colors: Array<Color> = [
+		0x21181b,
+		0x3b2027,
+		0x7d3833,
+		0xab5130,
+		0xcf752b,
+		0xf0b541,
+		0xffee83,
+		0xc8d45d,
+		0x63ab3f,
+		0x3b7d4f,
+		0x2f5753,
+		0x283540,
+		0x1b1f21,
+		0x2b2b45,
+		0x3a3f5e,
+		0x4c6885,
+		0x4fa4b8,
+		0x92e8c0,
+		0xf5ffe8,
+		0xdfe0e8,
+		0xa3a7c2,
+		0x686f99,
+		0x404973,
+		0x2c354d,
+		0x14182e,
+		0x4b1d52,
+		0x692464,
+		0x9c2a70,
+		0xcc2f7b,
+		0xff5277,
+		0xffc2a1,
+		0xff8933,
+		0xe64539,
+		0xad2f45,
+		0x781d4f,
+		0x4f1d4c,
+		0x291d2b,
+		0x3d2936,
+		0x52333f,
+		0x8f4d57,
+		0xbd6a62,
+		0xffae70,
+		0x4d494d,
+		0x757161,
+		0x8696a2,
+	];
+
+	/**
+		TODO
+		1. Store shades for various colors
+	**/
+}
