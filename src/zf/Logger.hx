acf2e9fb336f1852688fac90427ca9e523fe18b8
Eric Ng <ericnjf@gmail.com>
2021-06-10 23:42:11 +0800

    feat/Logger: add tag as param to replace the auto location String

--- src/zf/Logger.hx
+++ src/zf/Logger.hx
@@ -4,50 +4,58 @@ import haxe.macro.PositionTools;
 import haxe.macro.Context;
 
 class Logger {
-	macro public static function error(msg: String) {
+	macro public static function error(msg: String, tag: String = null) {
 #if (!loggingLevel || loggingLevel < 10)
 		return macro {};
 #else
-		var location = PositionTools.toLocation(Context.currentPos());
-		var locationString = location.file + ":" + location.range.start.line;
+		if (tag == null) {
+			var location = PositionTools.toLocation(Context.currentPos());
+			tag = location.file + ":" + location.range.start.line;
+		}
 		return macro {
-			haxe.Log.trace('${locationString}: [Error] ${msg}', null);
+			haxe.Log.trace('${tag}: [Error] ${msg}', null);
 		}
 #end
 	}
 
-	macro public static function warn(msg: String) {
+	macro public static function warn(msg: String, tag: String = null) {
 #if (!loggingLevel || loggingLevel < 15)
 		return macro {};
 #else
-		var location = PositionTools.toLocation(Context.currentPos());
-		var locationString = location.file + ":" + location.range.start.line;
+		if (tag == null) {
+			var location = PositionTools.toLocation(Context.currentPos());
+			tag = location.file + ":" + location.range.start.line;
+		}
 		return macro {
-			haxe.Log.trace('${locationString}: [Warn] ${msg}', null);
+			haxe.Log.trace('${tag}: [Warn] ${msg}', null);
 		}
 #end
 	}
 
-	macro public static function info(msg: String) {
+	macro public static function info(msg: String, tag: String = null) {
 #if (!loggingLevel || loggingLevel < 20)
 		return macro {};
 #else
-		var location = PositionTools.toLocation(Context.currentPos());
-		var locationString = location.file + ":" + location.range.start.line;
+		if (tag == null) {
+			var location = PositionTools.toLocation(Context.currentPos());
+			tag = location.file + ":" + location.range.start.line;
+		}
 		return macro {
-			haxe.Log.trace('${locationString}: [Info] ${msg}', null);
+			haxe.Log.trace('${tag}: [Info] ${msg}', null);
 		}
 #end
 	}
 
-	macro public static function debug(msg: String) {
+	macro public static function debug(msg: String, tag: String = null) {
 #if (!loggingLevel || loggingLevel < 30)
 		return macro {};
 #else
-		var location = PositionTools.toLocation(Context.currentPos());
-		var locationString = location.file + ":" + location.range.start.line;
+		if (tag == null) {
+			var location = PositionTools.toLocation(Context.currentPos());
+			tag = location.file + ":" + location.range.start.line;
+		}
 		return macro {
-			haxe.Log.trace('${locationString}: [Debug] ${msg}', null);
+			haxe.Log.trace('${tag}: [Debug] ${msg}', null);
 		}
 #end
 	}
