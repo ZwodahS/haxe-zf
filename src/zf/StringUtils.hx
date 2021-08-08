58835e29ac0fb32ef9ce1d163d7ded5b5deb4764
Eric Ng <ericnjf@gmail.com>
2021-08-08 16:37:19 +0800

    feat/StringUtils: add a equal function to be used for partial

--- src/zf/StringUtils.hx
+++ src/zf/StringUtils.hx
@@ -7,4 +7,12 @@ class StringUtils {
 		if (split.length == 1) return split[0];
 		return split[0] + '.' + split[1].substring(0, dp);
 	}
+
+	/**
+		For passing of function around rather than creating a function.
+		i.e. StringUtils.equals.bind("#") instead of creating a function.
+	**/
+	public static function equals(v1: String, v2: String): Bool {
+		return v1 == v2;
+	}
 }
