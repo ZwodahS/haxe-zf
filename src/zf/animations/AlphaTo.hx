dac44623c9fc8e7ccc1c377c063bf417c6c5d1d7
Eric Ng <ericnjf@gmail.com>
2021-08-11 17:02:39 +0800

    fix/animations: fix bug in AlphaTo

--- src/zf/animations/AlphaTo.hx
+++ src/zf/animations/AlphaTo.hx
@@ -17,10 +17,9 @@ class AlphaTo extends Animation {
 	}
 
 	override public function update(dt: Float) {
-		if (this.isDone()) {
+		if (this.isDone() || dt == 0) {
 			return;
 		}
-
 		var sign = this.object.alpha > this.alphaTo ? -1 : 1;
 		var delta = this.alphaSpeed * dt * sign;
 		if (delta < 0) {
