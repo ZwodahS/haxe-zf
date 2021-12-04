148dede95aab206a0ef687d175ce5407319a8eb5
Eric Ng <ericnjf@gmail.com>
2021-12-05 00:16:13 +0800

    feat/TileBoxFactory: add minWidth/maxWidth method

--- src/zf/ui/TileBoxFactory.hx
+++ src/zf/ui/TileBoxFactory.hx
@@ -9,6 +9,9 @@ class TileBoxFactory {
 	var borderBottom: Int;
 	var tile: h2d.Tile;
 
+	public var minWidth(get, never): Int;
+	public var minHeight(get, never): Int;
+
 	public function new(tile: h2d.Tile, borderL: Int, borderT: Int, ?borderR: Int, ?borderB: Int) {
 		this.borderLeft = borderL;
 		this.borderRight = (borderR != null) ? borderR : borderL;
@@ -17,11 +20,19 @@ class TileBoxFactory {
 		this.tile = tile;
 	}
 
-	public function make(boxSize: Point2i): h2d.Drawable {
+	public function make(boxSize: Point2i): h2d.ScaleGrid {
 		var obj = new h2d.ScaleGrid(this.tile, this.borderLeft, this.borderTop, this.borderRight,
 			this.borderBottom);
 		obj.width = boxSize.x;
 		obj.height = boxSize.y;
 		return obj;
+	}
+
+	public function get_minWidth(): Int {
+		return this.borderLeft + this.borderRight + 1;
+	}
+
+	public function get_minHeight(): Int {
+		return this.borderTop + this.borderBottom + 1;
 	}
 }
