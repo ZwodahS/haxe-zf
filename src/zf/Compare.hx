f4835052c2ea8250021e08033dee67ac74f8ed98
Eric Ng <ericnjf@gmail.com>
2022-07-07 15:01:16 +0800

    feat/Compare: add some compare function for array sort

---
+++ src/zf/Compare.hx
@@ -0,0 +1,29 @@
+package zf;
+
+/**
+	Standard compare functions for sorting
+
+	Usage:
+
+	var arr1: Array<String> = [ .... ]
+
+	// positive number for ascending, negative for descending, if 0 always returns 0, i.e. no sort
+	arr1.sort(Compare.string.bind(true, 1));
+**/
+class Compare {
+	/**
+		Sort string
+	**/
+	public static function string(ignoreCase: Bool, direction: Int, str1: String, str2: String): Int {
+		if (ignoreCase == true) {
+			str1 = str1.toUpperCase();
+			str2 = str2.toUpperCase();
+		}
+
+		if (direction == 0 || str1 == str2) return 0;
+		direction = direction > 0 ? 1 : -1;
+
+		if (str1 > str2) return direction;
+		return -(direction);
+	}
+}
