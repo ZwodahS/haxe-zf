a39efe683b50c8df43500feadbf30c4d7fa32a46
Eric Ng <ericnjf@gmail.com>
2022-09-13 11:48:12 +0800

    fix/ScaleGridFactory: add color

--- src/zf/ui/ScaleGridFactory.hx
+++ src/zf/ui/ScaleGridFactory.hx
@@ -10,24 +10,33 @@ class ScaleGridFactory {
 	var borderBottom: Int;
 	var tile: h2d.Tile;
 
+	public var color: Null<Color>;
+
 	public var tiling: Bool = false;
 
 	public var minWidth(get, never): Int;
 	public var minHeight(get, never): Int;
 
-	public function new(tile: h2d.Tile, borderL: Int, borderT: Int, ?borderR: Int, ?borderB: Int) {
+	public function new(tile: h2d.Tile, borderL: Int, borderT: Int, ?borderR: Int, ?borderB: Int,
+			color: Null<Color> = null) {
 		this.borderLeft = borderL;
 		this.borderRight = (borderR != null) ? borderR : borderL;
 		this.borderTop = borderT;
 		this.borderBottom = (borderB != null) ? borderB : borderT;
 		this.tile = tile;
+		this.color = color;
 	}
 
-	public function make(boxSize: Point2i): ScaleGrid {
+	public function make(boxSize: Point2i, color: Null<Color> = null): ScaleGrid {
 		var obj = new ScaleGrid(this.tile, this.borderLeft, this.borderTop, this.borderRight, this.borderBottom);
 		obj.width = boxSize.x;
 		obj.height = boxSize.y;
 		obj.tiling = this.tiling;
+		if (color != null) {
+			obj.color = h3d.Vector.fromColor(color);
+		} else if (this.color != null) {
+			obj.color = h3d.Vector.fromColor(this.color);
+		}
 		return obj;
 	}
 
