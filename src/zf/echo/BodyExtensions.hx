b18b6195df4a839d500bdfa194d5577cf0323e8a
Eric Ng <ericnjf@gmail.com>
2022-10-11 11:11:25 +0800

    impl/Extensions: add echo extensions

---
+++ src/zf/echo/BodyExtensions.hx
@@ -0,0 +1,39 @@
+package zf.echo;
+
+import echo.data.Data.CollisionData;
+
+#if echo
+class BodyExtensions {
+	public static function getCollisionData(body1: echo.Body, body2: echo.Body = null, shape2: echo.Shape = null,
+			shapes2: Array<echo.Shape> = null): CollisionData {
+		if (body2 != null) {
+			for (s1 in body1.shapes) {
+				for (s2 in body2.shapes) {
+					final c = s1.collides(s2);
+					if (c != null) return c;
+				}
+			}
+		}
+
+		if (shape2 != null) {
+			for (s1 in body1.shapes) {
+				final c = s1.collides(shape2);
+				if (c != null) return c;
+			}
+		}
+
+		if (shapes2 != null) {
+			for (s1 in body1.shapes) {
+				for (s2 in shapes2) {
+					final c = s1.collides(s2);
+					if (c != null) return c;
+				}
+			}
+		}
+
+		return null;
+	}
+}
+#else
+class BodyExtensions {}
+#end
---
+++ src/zf/echo/ShapeExtensions.hx
@@ -0,0 +1,33 @@
+package zf.echo;
+
+import echo.data.Data.CollisionData;
+
+#if echo
+class ShapeExtensions {
+	public static function getCollisionData(shape1: echo.Shape, body2: echo.Body = null, shape2: echo.Shape = null,
+			shapes2: Array<echo.Shape> = null): CollisionData {
+		if (body2 != null) {
+			for (s2 in body2.shapes) {
+				final c = shape1.collides(s2);
+				if (c != null) return c;
+			}
+		}
+
+		if (shape2 != null) {
+			final c = shape1.collides(shape2);
+			if (c != null) return c;
+		}
+
+		if (shapes2 != null) {
+			for (s2 in shapes2) {
+				final c = shape1.collides(s2);
+				if (c != null) return c;
+			}
+		}
+
+		return null;
+	}
+}
+#else
+class ShapeExtensions {}
+#end
