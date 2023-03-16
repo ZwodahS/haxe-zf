b92dedff90b04ee63e3bd6a9d1a30e77706edc4d
Eric Ng <ericnjf@gmail.com>
2023-03-16 15:56:50 +0800

    impl/Logger: upgrade Logger to output to file

--- src/zf/Logger.hx
+++ src/zf/Logger.hx
@@ -5,96 +5,179 @@ import haxe.macro.Context;
 
 import zf.exceptions.AssertionFail;
 
+interface LoggerInf {
+	public function print(logLevel: Int, message: String): Void;
+	public function dispose(): Void;
+}
+
+class ConsoleLogger implements LoggerInf {
+	public var minLogLevel: Null<Int> = null;
+	public var maxLogLevel: Null<Int> = null;
+
+	public function new() {}
+
+	public function print(logLevel: Int, message: String) {
+		if (this.minLogLevel != null && logLevel < this.minLogLevel) return;
+		if (this.maxLogLevel != null && logLevel > this.maxLogLevel) return;
+		haxe.Log.trace(message, null);
+	}
+
+	public function dispose() {}
+}
+
+#if sys
+class FileLogger implements LoggerInf {
+	public var minLogLevel: Null<Int> = null;
+	public var maxLogLevel: Null<Int> = null;
+	public var file: sys.io.FileOutput;
+
+	public function new(path: String) {
+		try {
+			this.file = sys.io.File.append(path, false);
+		} catch (e) {
+			this.file = null;
+		}
+		// @todo file rotate
+	}
+
+	public function print(logLevel: Int, message: String) {
+		if (this.file == null) return;
+		if (this.minLogLevel != null && logLevel < this.minLogLevel) return;
+		if (this.maxLogLevel != null && logLevel > this.maxLogLevel) return;
+		this.file.writeString(message + "\n");
+		this.file.flush();
+	}
+
+	public function dispose() {
+		if (this.file == null) return;
+		try {
+			this.file.close();
+		} catch (e) {}
+	}
+}
+#end
+
 /**
 	@stage:stable
 **/
 class Logger {
 	public static var PadLength: Int = 15;
 
+	public static var loggers: Array<LoggerInf>;
+
+	public static function init() {
+		if (Logger.loggers != null) return;
+		Logger.loggers = [];
+	}
+
+	public static function addConsoleLogger(minLevel: Null<Int> = null, maxLevel: Null<Int> = null) {
+		final logger = new ConsoleLogger();
+		logger.minLogLevel = minLevel;
+		logger.maxLogLevel = maxLevel;
+		Logger.loggers.push(logger);
+	}
+
+	public static function addFileLogger(path: String, minLevel: Null<Int>, maxLevel: Null<Int>) {
+#if sys
+		final logger = new FileLogger(path);
+		logger.minLogLevel = minLevel;
+		logger.maxLogLevel = maxLevel;
+		Logger.loggers.push(logger);
+#end
+	}
+
+	public static function dispose() {
+		if (Logger.loggers == null) return;
+		for (l in Logger.loggers) l.dispose();
+	}
+
+	public static function print(level: Int, message: String) {
+		if (Logger.loggers == null) return;
+		for (l in Logger.loggers) l.print(level, message);
+	}
+
+	/**
+		Error
+		Log level 100
+	**/
 	macro public static function error(msg: ExprOf<String>, tag: String = null) {
-#if (!loggingLevel || loggingLevel < 10)
-		return macro {};
-#else
 		if (tag == null) {
 			var location = PositionTools.toLocation(Context.currentPos());
 			tag = location.file + ":" + location.range.start.line;
 		}
 		return macro {
-			haxe.Log.trace(StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' Error: ' + $e{msg}, null);
+			Logger.print(100, StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' [Error] ' + $e{msg});
 		}
-#end
 	}
 
+	/**
+		Warn
+		Log level 50
+	**/
 	macro public static function warn(msg: ExprOf<String>, tag: String = null) {
-#if (!loggingLevel || loggingLevel < 15)
-		return macro {};
-#else
 		if (tag == null) {
 			var location = PositionTools.toLocation(Context.currentPos());
 			tag = location.file + ":" + location.range.start.line;
 		}
 		return macro {
-			haxe.Log.trace(StringTools.lpad($v{tag}, " ", Logger.PadLength) + '  Warn: ' + $e{msg}, null);
+			Logger.print(50, StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' [Warn] ' + $e{msg});
 		}
-#end
 	}
 
+	/**
+		Info
+		Log level 25
+	**/
 	macro public static function info(msg: ExprOf<String>, tag: String = null) {
-#if (!loggingLevel || loggingLevel < 20)
-		return macro {};
-#else
 		if (tag == null) {
 			var location = PositionTools.toLocation(Context.currentPos());
 			tag = location.file + ":" + location.range.start.line;
 		}
 		return macro {
-			haxe.Log.trace(StringTools.lpad($v{tag}, " ", Logger.PadLength) + '  Info: ' + $e{msg}, null);
+			Logger.print(25, StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' [Info] ' + $e{msg});
 		}
-#end
 	}
 
+	/**
+		mark part of the code is deprecated
+		Log level 40
+	**/
 	macro public static function deprecated() {
-#if !debug
-		return macro {};
-#else
 		var location = PositionTools.toLocation(Context.currentPos());
 		final tag = location.file + ":" + location.range.start.line;
 		return macro {
-			haxe.Log.trace(StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' Debug: this code is deprecated.');
+			Logger.print(40, 'Debug: This code is deprecated');
 		}
-#end
 	}
 
+	/**
+		debug
+		Log level 0
+	**/
 	macro public static function debug(msg: ExprOf<String>, tag: String = null) {
-#if (!loggingLevel || loggingLevel < 30)
-		return macro {};
-#else
 		if (tag == null) {
 			var location = PositionTools.toLocation(Context.currentPos());
 			tag = location.file + ":" + location.range.start.line;
 		}
 		return macro {
-			haxe.Log.trace(StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' Debug: ' + $e{msg}, null);
+			Logger.print(0, StringTools.lpad($v{tag}, " ", Logger.PadLength) + ' [Debug] ' + $e{msg});
 		}
-#end
 	}
 
 	inline public static function exception(e: haxe.Exception, stackItems: Array<haxe.CallStack.StackItem> = null) {
-#if debug
 		if (Std.isOfType(e, AssertionFail)) {
-			haxe.Log.trace(e.message, null);
+			Logger.print(100, 'Exception: ' + e.message);
 		} else {
 			for (es in haxe.CallStack.exceptionStack()) trace(es);
-			haxe.Log.trace(e, null);
+			Logger.print(100, 'Exception: ' + e);
 			if (stackItems != null) {
 				for (s in stackItems) {
-					haxe.Log.trace('Called from ${stackItemToString(s)}', null);
+					Logger.print(100, 'Called from ${stackItemToString(s)}');
 				}
 			} else {
-				haxe.Log.trace(e.stack, null);
+				Logger.print(100, e.stack.toString());
 			}
 		}
-#end
 	}
 
 	public static function stackItemToString(s: haxe.CallStack.StackItem) {
