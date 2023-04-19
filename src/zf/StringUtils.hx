e86b765139a67786df216ff43d87dd1050cbcc14
Eric Ng <ericnjf@gmail.com>
2023-04-19 21:21:13 +0800

    fix/StringUtils: fix formatFloat not parsing 0 dp correctly

--- src/zf/StringUtils.hx
+++ src/zf/StringUtils.hx
@@ -7,7 +7,7 @@ class StringUtils {
 	inline public static function formatFloat(v: Float, dp: Int = 0): String {
 		var str = '${v}';
 		var split = str.split('.');
-		if (split.length == 1) return split[0];
+		if (split.length == 1 || dp == 0) return split[0];
 		return split[0] + '.' + split[1].substring(0, dp);
 	}
 
