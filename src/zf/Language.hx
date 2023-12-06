72a3c414b2d01a5242c655a520851335179ccfac
Eric Ng <ericnjf@gmail.com>
2023-12-06 10:43:44 +0800

    impl/lang: add Language

--- src/zf/Language.hx
+++ src/zf/Language.hx
@@ -8,10 +8,11 @@ typedef LanguageConfig = {
 class Language {
 	public static final English: LanguageConfig = {id: "en", name: "English"};
 	public static final SimplifiedChinese: LanguageConfig = {id: "zh-cn", name: "简体中文"};
+	public static final TraditionalChinese: LanguageConfig = {id: "zh-tw", name: "繁體中文"};
 	public static final Japanese: LanguageConfig = {id: "ja", name: "日本語"};
 	public static final German: LanguageConfig = {id: "de", name: "Deutsch"};
 	public static final Spanish: LanguageConfig = {id: "es", name: "Español"};
-	public static final Korean: LanguageConfig = {id: "ko", name: "한국인"};
+	public static final Korean: LanguageConfig = {id: "ko", name: "한국어"};
 	public static final French: LanguageConfig = {id: "fr", name: "Français"};
 	public static final Italian: LanguageConfig = {id: "it", name: "Italiano"};
 }
