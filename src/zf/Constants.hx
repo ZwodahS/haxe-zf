d4bac57449ab109925ba02f565f97956f71b4ef4
Eric Ng <ericnjf@gmail.com>
2024-02-06 13:21:59 +0800

    impl/GridRange: add grid range dynamic

--- src/zf/Constants.hx
+++ src/zf/Constants.hx
@@ -6,4 +6,20 @@ package zf;
 class Constants {
 	public static final MaxInt32: Int = 0x7FFFFFFE;
 	public static final SeedMax: Int = MaxInt32 - 1;
+
+
+	/**
+		Tue 12:59:41 30 Jan 2024
+		Not sure where to put this
+	**/
+	public static function gridRangeDynamic(): Dynamic {
+		return {
+			Adjacent: zf.ds.GridRange.Adjacent,
+			Around: zf.ds.GridRange.Around,
+			Row: zf.ds.GridRange.Row,
+			Column: zf.ds.GridRange.Column,
+			RowColumn: zf.ds.GridRange.RowColumn,
+			All: zf.ds.GridRange.All,
+		}
+	}
 }
